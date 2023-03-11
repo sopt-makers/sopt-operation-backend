d8ee2c0c7e43549b7e68408112a7e095d8975418
thguss <thgus345@naver.com>
2023-03-11 14:18:14 +0900

    [INIT] 프로젝트 초기 세팅

---
+++ src/main/java/org/sopt/makers/operation/OperationServerApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.makers.operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationServerApplication.class, args);
+	}
+
+}
