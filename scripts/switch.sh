560a4e16c7d272363305bdf1d7900017ffa0ddc4
thguss <thgus345@naver.com>
2023-03-13 02:56:01 +0900

    [FIX] 포트포워딩

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -22,6 +22,7 @@ sudo service nginx reload
 
 echo "> Nginx reloaded."
 
+sudo iptables -t nat -D PREROUTING 1
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port ${TARGET_PORT}
 
 echo "> Forward ${TARGET_PORT} port"
