01f03434ff30df3e0b723c7cb69bd8f9f3cb04fe
thguss <thgus345@naver.com>
2023-03-22 22:20:08 +0900

    [ADD] JpaAuditing 기능 관련 추가: 생성 및 수정 날짜

---
+++ src/main/java/org/sopt/makers/operation/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package org.sopt.makers.operation.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseEntity {
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdDate;
+
+	@LastModifiedDate
+	private LocalDateTime lastModifiedDate;
+}
