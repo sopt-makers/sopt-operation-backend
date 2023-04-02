5aa120d50883108f6bd3ad174d22655454511180
thguss <thgus345@naver.com>
2023-04-02 18:19:16 +0900

    [CHORE] ApiResponse 형식 추가 #19

---
+++ src/main/java/org/sopt/makers/operation/common/ApiResponse.java
@@ -0,0 +1,16 @@
+package org.sopt.makers.operation.common;
+
+public record ApiResponse(
+	boolean success,
+	String message,
+	Object data
+) {
+
+	public static ApiResponse success(String message, Object data) {
+		return new ApiResponse(true, message, data);
+	}
+
+	public static ApiResponse fail(String message) {
+		return new ApiResponse(false, message, null);
+	}
+}
