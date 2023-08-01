553c39ffa5963606d874e6a271b73d5961fdfe81
thguss <thgus345@naver.com>
2023-08-01 18:59:06 +0900

    [REFACT] 날짜 포맷 IS 8601 변경

--- src/main/java/org/sopt/makers/operation/controller/ExceptionController.java
+++ src/main/java/org/sopt/makers/operation/controller/ExceptionController.java
@@ -1,8 +1,13 @@
 package org.sopt.makers.operation.controller;
 
+import static org.sopt.makers.operation.common.ApiResponse.*;
+import static org.sopt.makers.operation.common.ExceptionMessage.*;
+import static org.springframework.http.HttpStatus.*;
+
+import java.time.format.DateTimeParseException;
+
 import org.sopt.makers.operation.common.ApiResponse;
 import org.sopt.makers.operation.exception.*;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,32 +16,37 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionController {
     @ExceptionHandler(AdminFailureException.class)
     public ResponseEntity<ApiResponse> authFailureException (AdminFailureException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.fail(ex.getMessage()));
+        return ResponseEntity.status(BAD_REQUEST).body(fail(ex.getMessage()));
     }
 
     @ExceptionHandler(TokenException.class)
-    public ResponseEntity<ApiResponse> TokenException (TokenException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.fail(ex.getMessage()));
+    public ResponseEntity<ApiResponse> tokenException (TokenException ex) {
+        return ResponseEntity.status(BAD_REQUEST).body(fail(ex.getMessage()));
     }
 
     @ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<ApiResponse> IllegalStateExceptionException (IllegalStateException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.fail(ex.getMessage()));
+    public ResponseEntity<ApiResponse> illegalStateExceptionException (IllegalStateException ex) {
+        return ResponseEntity.status(BAD_REQUEST).body(fail(ex.getMessage()));
     }
 
     @ExceptionHandler(MemberException.class)
-    public ResponseEntity<ApiResponse> MemberException (MemberException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.fail(ex.getMessage()));
+    public ResponseEntity<ApiResponse> memberException (MemberException ex) {
+        return ResponseEntity.status(NOT_FOUND).body(fail(ex.getMessage()));
     }
 
     @ExceptionHandler(LectureException.class)
-    public ResponseEntity<ApiResponse> LectureException (LectureException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.fail(ex.getMessage()));
+    public ResponseEntity<ApiResponse> lectureException (LectureException ex) {
+        return ResponseEntity.status(NOT_FOUND).body(fail(ex.getMessage()));
     }
 
     @ExceptionHandler(SubLectureException.class)
-    public ResponseEntity<ApiResponse> SubLectureException (SubLectureException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.fail(ex.getMessage()));
+    public ResponseEntity<ApiResponse> subLectureException (SubLectureException ex) {
+        return ResponseEntity.status(NOT_FOUND).body(fail(ex.getMessage()));
+    }
+
+    @ExceptionHandler(DateTimeParseException.class)
+    public ResponseEntity<ApiResponse> dateTimeParseException () {
+        return ResponseEntity.status(BAD_REQUEST).body(fail(FAULT_DATE_FORMATTER.getName()));
     }
 
 }
