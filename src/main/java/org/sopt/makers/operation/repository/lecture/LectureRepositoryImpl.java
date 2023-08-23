ac4181c1e5da51e05d04f9f6c989c061786e10bc
dragontaek-lee <dragontaek98@naver.com>
2023-08-24 04:09:55 +0900

    [FIX] 현재 세션 기존 쿼리 삭제 #133

--- src/main/java/org/sopt/makers/operation/repository/lecture/LectureRepositoryImpl.java
+++ src/main/java/org/sopt/makers/operation/repository/lecture/LectureRepositoryImpl.java
@@ -3,17 +3,11 @@ package org.sopt.makers.operation.repository.lecture;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import lombok.val;
-import org.sopt.makers.operation.dto.lecture.LectureSearchCondition;
 import org.sopt.makers.operation.entity.Part;
 import org.sopt.makers.operation.entity.lecture.Lecture;
 import org.sopt.makers.operation.config.GenerationConfig;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
-import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Objects.*;
@@ -25,27 +19,6 @@ import static org.sopt.makers.operation.entity.lecture.QLecture.*;
 public class LectureRepositoryImpl implements LectureCustomRepository {
     private final JPAQueryFactory queryFactory;
     private final GenerationConfig generationConfig;
-
-    @Override
-    public List<Lecture> searchLecture(LectureSearchCondition lectureSearchCondition) {
-        val now = LocalDateTime.now();
-        val today = now.toLocalDate();
-        val startOfDay = today.atStartOfDay();
-        val endOfDay = LocalDateTime.of(today, LocalTime.MAX);
-
-        List<Part> partList = Arrays.asList(lectureSearchCondition.part(), Part.ALL);
-
-        return queryFactory
-                .selectFrom(lecture)
-                .where(
-                        lecture.part.in(partList),
-                        lecture.generation.eq(generationConfig.getCurrentGeneration()),
-                        lecture.startDate.between(startOfDay, endOfDay)
-                )
-                .orderBy(lecture.startDate.asc())
-                .fetch();
-    }
-
     @Override
     public List<Lecture> findLectures(int generation, Part part) {
         return queryFactory
