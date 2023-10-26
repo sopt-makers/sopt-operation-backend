9559942b08a6234ef571fdcfd499dd676e84e799
dragontaek-lee <dragontaek98@naver.com>
2023-10-27 04:12:29 +0900

    [FIX] 미사용 import 제거 #171

--- src/main/java/org/sopt/makers/operation/controller/web/AlarmController.java
+++ src/main/java/org/sopt/makers/operation/controller/web/AlarmController.java
@@ -2,12 +2,9 @@ package org.sopt.makers.operation.controller.web;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import lombok.val;
 import org.sopt.makers.operation.common.ApiResponse;
 import org.sopt.makers.operation.dto.alarm.AlarmSendRequestDTO;
-import org.sopt.makers.operation.dto.lecture.LectureRequestDTO;
 import org.sopt.makers.operation.service.AlarmService;
-import org.sopt.makers.operation.service.LectureService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
