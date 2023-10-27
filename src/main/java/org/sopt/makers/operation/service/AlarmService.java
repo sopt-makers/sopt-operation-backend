0846955c4875116f3c07f3c55ca83d864df5f6c1
thguss <thgus345@naver.com>
2023-10-27 11:56:32 +0900

    [FIX] 탭 사이즈 통일

--- src/main/java/org/sopt/makers/operation/service/AlarmService.java
+++ src/main/java/org/sopt/makers/operation/service/AlarmService.java
@@ -13,10 +13,15 @@ import org.sopt.makers.operation.entity.alarm.Status;
 import org.springframework.data.domain.Pageable;
 
 public interface AlarmService {
-    void sendAdmin(AlarmSendRequestDTO requestDTO);
-    void send(String title, String content, List<String> targetList, Attribute attribute, String link);
+	void sendAdmin(AlarmSendRequestDTO requestDTO);
+
+	void send(String title, String content, List<String> targetList, Attribute attribute, String link);
+
 	Long createAlarm(AlarmRequestDTO requestDTO);
+
 	AlarmsResponseDTO getAlarms(Integer generation, Part part, Status status, Pageable pageable);
+
 	AlarmResponseDTO getAlarm(Long alarmId);
+
 	void deleteAlarm(Long alarmId);
 }
