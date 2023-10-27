92e8a0692302150124281033e4c13766bfe1f400
thguss <thgus345@naver.com>
2023-10-27 11:57:37 +0900

    [FIX] 탭 사이즈 통일

--- src/main/java/org/sopt/makers/operation/service/AlarmServiceImpl.java
+++ src/main/java/org/sopt/makers/operation/service/AlarmServiceImpl.java
@@ -3,6 +3,7 @@ package org.sopt.makers.operation.service;
 import static org.sopt.makers.operation.common.ExceptionMessage.*;
 
 import lombok.RequiredArgsConstructor;
+
 import org.sopt.makers.operation.dto.alarm.AlarmInactiveListResponseDTO;
 import org.sopt.makers.operation.dto.alarm.AlarmSendRequestDTO;
 import org.sopt.makers.operation.dto.alarm.AlarmSendResponseDTO;
@@ -22,9 +23,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
 import lombok.val;
 
 import javax.persistence.EntityNotFoundException;
+
 import java.util.*;
 
 import org.sopt.makers.operation.dto.alarm.AlarmRequestDTO;
@@ -36,128 +39,129 @@ import org.springframework.data.domain.Pageable;
 @Service
 @RequiredArgsConstructor
 public class AlarmServiceImpl implements AlarmService {
-    @Value("${notification.key}")
-    private String key;
+	@Value("${notification.key}")
+	private String key;
 
-    @Value("${notification.url}")
-    private String host;
+	@Value("${notification.url}")
+	private String host;
 
-    @Value("${sopt.makers.playground.server}")
-    private String playGroundURI;
+	@Value("${sopt.makers.playground.server}")
+	private String playGroundURI;
 
-    @Value("${sopt.makers.playground.token}")
-    private String playGroundToken;
+	@Value("${sopt.makers.playground.token}")
+	private String playGroundToken;
 
-    @Value("${sopt.current.generation}")
-    private int currentGeneration;
+	@Value("${sopt.current.generation}")
+	private int currentGeneration;
 
-    private final RestTemplate restTemplate = new RestTemplate();
-    private final AlarmRepository alarmRepository;
-    private final MemberRepository memberRepository;
+	private final RestTemplate restTemplate = new RestTemplate();
+	private final AlarmRepository alarmRepository;
+	private final MemberRepository memberRepository;
 
-    private final List<String> appLinkList = Arrays.asList(
-            "home", "home/notification", "home/mypage", "home/attendance",
-            "home/attendance/attendance-modal", "home/soptamp",
-            "home/soptamp/entire-ranking", "home/soptamp/current-generation-ranking"
-    );
+	private final List<String> appLinkList = Arrays.asList(
+		"home", "home/notification", "home/mypage", "home/attendance",
+		"home/attendance/attendance-modal", "home/soptamp",
+		"home/soptamp/entire-ranking", "home/soptamp/current-generation-ranking"
+	);
 
-    private final List<String> webLinkList = Arrays.asList(
-            "https://playground.sopt.org/members",
-            "https://playground.sopt.org/group"
-    );
+	private final List<String> webLinkList = Arrays.asList(
+		"https://playground.sopt.org/members",
+		"https://playground.sopt.org/group"
+	);
 
-    @Override
-    @Transactional
-    public void sendAdmin(AlarmSendRequestDTO requestDTO) {
-        val alarm = alarmRepository.findById(requestDTO.alarmId())
-                .orElseThrow(() -> new EntityNotFoundException(INVALID_ALARM.getName()));
+	@Override
+	@Transactional
+	public void sendAdmin(AlarmSendRequestDTO requestDTO) {
+		val alarm = alarmRepository.findById(requestDTO.alarmId())
+			.orElseThrow(() -> new EntityNotFoundException(INVALID_ALARM.getName()));
 
-        if (alarm.getStatus().equals(Status.AFTER)) {
-            throw new AlarmException(ALREADY_SEND_ALARM.getName());
-        }
+		if (alarm.getStatus().equals(Status.AFTER)) {
+			throw new AlarmException(ALREADY_SEND_ALARM.getName());
+		}
 
-        val targetList = alarm.getTargetList();
+		val targetList = alarm.getTargetList();
 
-        List<String> targetIdList;
-        if (targetList.size() != 0) {
-            targetIdList = alarm.getTargetList();
-        } else {
-            if (alarm.getIsActive()) {
-                targetIdList = extractCurrentTargetList(alarm.getPart());
-            } else {
-                targetIdList = extractInactiveTargetList(currentGeneration, alarm.getPart())
-                        .memberIds().stream()
-                        .map(String::valueOf).toList();
-            }
-        }
+		List<String> targetIdList;
+		if (targetList.size() != 0) {
+			targetIdList = alarm.getTargetList();
+		} else {
+			if (alarm.getIsActive()) {
+				targetIdList = extractCurrentTargetList(alarm.getPart());
+			} else {
+				targetIdList = extractInactiveTargetList(currentGeneration, alarm.getPart())
+					.memberIds().stream()
+					.map(String::valueOf).toList();
+			}
+		}
 
-        send(alarm.getTitle(), alarm.getContent(), targetIdList, alarm.getAttribute(), alarm.getLink());
-        alarm.updateStatus();
-    }
+		send(alarm.getTitle(), alarm.getContent(), targetIdList, alarm.getAttribute(), alarm.getLink());
+		alarm.updateStatus();
+	}
 
-    private List<String> extractCurrentTargetList(Part part) {
-        val memberList = memberRepository.search(new MemberSearchCondition(part, currentGeneration));
-        return memberList.stream()
-                .map(member -> String.valueOf(member.getPlaygroundId()))
-                .toList();
-    }
+	private List<String> extractCurrentTargetList(Part part) {
+		val memberList = memberRepository.search(new MemberSearchCondition(part, currentGeneration));
+		return memberList.stream()
+			.map(member -> String.valueOf(member.getPlaygroundId()))
+			.toList();
+	}
 
-    private AlarmInactiveListResponseDTO extractInactiveTargetList(int generation, Part part) {
-        val getInactiveUserURL = playGroundURI + "/internal/api/v1/members/inactivity?generation=" + generation + "&part=" + part;
+	private AlarmInactiveListResponseDTO extractInactiveTargetList(int generation, Part part) {
+		val getInactiveUserURL =
+			playGroundURI + "/internal/api/v1/members/inactivity?generation=" + generation + "&part=" + part;
 
-        val headers = new HttpHeaders();
-        headers.add("content-type", "application/json;charset=UTF-8");
-        headers.add("Authorization", playGroundToken);
+		val headers = new HttpHeaders();
+		headers.add("content-type", "application/json;charset=UTF-8");
+		headers.add("Authorization", playGroundToken);
 
-        val entity = new HttpEntity<>(null, headers);
+		val entity = new HttpEntity<>(null, headers);
 
-        try {
-            val response = restTemplate.exchange(
-                    getInactiveUserURL,
-                    HttpMethod.GET,
-                    entity,
-                    AlarmInactiveListResponseDTO.class
-            );
+		try {
+			val response = restTemplate.exchange(
+				getInactiveUserURL,
+				HttpMethod.GET,
+				entity,
+				AlarmInactiveListResponseDTO.class
+			);
 
-            return response.getBody();
-        } catch (Exception e) {
-            throw new AlarmException(FAIL_INACTIVE_USERS.getName());
-        }
-    }
+			return response.getBody();
+		} catch (Exception e) {
+			throw new AlarmException(FAIL_INACTIVE_USERS.getName());
+		}
+	}
 
-    @Override
-    public void send(String title, String content, List<String> targetList, Attribute attribute, String link) {
-        val alarmRequest = new HashMap<>();
-        
-        alarmRequest.put("userIds", targetList);
-        alarmRequest.put("title", title);
-        alarmRequest.put("content", content);
-        alarmRequest.put("category", attribute);
+	@Override
+	public void send(String title, String content, List<String> targetList, Attribute attribute, String link) {
+		val alarmRequest = new HashMap<>();
 
-        if (Objects.nonNull(link)) {
-            if (appLinkList.contains(link)) {
-                alarmRequest.put("appLink", link);
-            } else {
-                alarmRequest.put("webLink", link);
-            }
-        }
+		alarmRequest.put("userIds", targetList);
+		alarmRequest.put("title", title);
+		alarmRequest.put("content", content);
+		alarmRequest.put("category", attribute);
 
-        val headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.add("action","send");
-        headers.add("transactionId",UUID.randomUUID().toString());
-        headers.add("service","operation");
-        headers.add("x-api-key", key);
+		if (Objects.nonNull(link)) {
+			if (appLinkList.contains(link)) {
+				alarmRequest.put("appLink", link);
+			} else {
+				alarmRequest.put("webLink", link);
+			}
+		}
 
-        val entity = new HttpEntity<>(alarmRequest, headers);
+		val headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+		headers.add("action", "send");
+		headers.add("transactionId", UUID.randomUUID().toString());
+		headers.add("service", "operation");
+		headers.add("x-api-key", key);
 
-        try {
-            restTemplate.postForEntity(host, entity, AlarmSendResponseDTO.class);
-        } catch (HttpClientErrorException e) {
-            throw new AlarmException(FAIL_SEND_ALARM.getName());
-        }
-    }
+		val entity = new HttpEntity<>(alarmRequest, headers);
+
+		try {
+			restTemplate.postForEntity(host, entity, AlarmSendResponseDTO.class);
+		} catch (HttpClientErrorException e) {
+			throw new AlarmException(FAIL_SEND_ALARM.getName());
+		}
+	}
 
 	@Override
 	@Transactional
