bbe6a4200fddaa4acfb62617bd47604a71cae856
thguss <thgus345@naver.com>
2024-01-19 11:05:43 +0900

    [TEST] 세션 컨트롤러 테스트

--- src/test/java/org/sopt/makers/operation/controller/web/LectureControllerTest.java
+++ src/test/java/org/sopt/makers/operation/controller/web/LectureControllerTest.java
@@ -1,9 +1,5 @@
 package org.sopt.makers.operation.controller.web;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,16 +9,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.sopt.makers.operation.dto.lecture.AttendanceRequestDTO;
 import org.sopt.makers.operation.dto.lecture.AttendanceResponseDTO;
-import org.sopt.makers.operation.dto.lecture.AttendancesStatusVO;
 import org.sopt.makers.operation.dto.lecture.LectureDetailResponseDTO;
 import org.sopt.makers.operation.dto.lecture.LectureRequestDTO;
 import org.sopt.makers.operation.dto.lecture.LectureResponseDTO;
-import org.sopt.makers.operation.dto.lecture.LectureResponseDTO.SubLectureVO;
 import org.sopt.makers.operation.dto.lecture.LecturesResponseDTO;
-import org.sopt.makers.operation.dto.lecture.LecturesResponseDTO.LectureVO;
-import org.sopt.makers.operation.entity.Part;
-import org.sopt.makers.operation.entity.lecture.Attribute;
-import org.sopt.makers.operation.entity.lecture.LectureStatus;
 import org.sopt.makers.operation.service.LectureService;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -33,6 +23,7 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 import static org.mockito.Mockito.*;
+import static org.sopt.makers.operation.fixture.LectureFixture.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.google.gson.Gson;
@@ -48,15 +39,6 @@ class LectureControllerTest {
 	private MockMvc mockMvc;
 
 	private final String DEFAULT_URI = "/api/v1/lectures";
-	private final String LECTURE_NAME = "테스트 이름";
-	private final Part LECTURE_PART = Part.ALL;
-	private final int LECTURE_GENERATION = 30;
-	private final String LECTURE_PLACE = "테스트 장소";
-	private final LocalDateTime NOW = LocalDateTime.now();
-	private final Attribute LECTURE_ATTRIBUTE = Attribute.ETC;
-	private final LectureStatus LECTURE_STATUS = LectureStatus.BEFORE;
-	private final int LIST_SIZE = 5;
-	private final String SUB_LECTURE_CODE = "code";
 
 	@BeforeEach
 	public void init() {
@@ -183,91 +165,6 @@ class LectureControllerTest {
 				.startDate(NOW.toString())
 				.endDate(NOW.plusHours(4).toString())
 				.attribute(LECTURE_ATTRIBUTE)
-				.build();
-	}
-
-	private long lectureId() {
-		return 0L;
-	}
-
-	private LecturesResponseDTO lecturesResponse() {
-		return new LecturesResponseDTO(LECTURE_GENERATION, lectures());
-	}
-
-	private List<LectureVO> lectures() {
-		return Stream.iterate(1, i -> i + 1).limit(LIST_SIZE)
-				.map(this::lecture).toList();
-	}
-
-	private LectureVO lecture(int i) {
-		return LectureVO.builder()
-				.lectureId(0L)
-				.name(LECTURE_NAME + i)
-				.partValue(Part.ALL)
-				.partName(Part.ALL.getName())
-				.startDate(NOW.plusHours(i).toString())
-				.endDate(NOW.plusHours(i + 4).toString())
-				.attributeValue(LECTURE_ATTRIBUTE)
-				.attributeName(LECTURE_ATTRIBUTE.getName())
-				.place(LECTURE_PLACE + i)
-				.attendances(attendancesStatus())
-				.build();
-	}
-
-	private LectureResponseDTO lectureResponse() {
-		return LectureResponseDTO.builder()
-				.lectureId(0L)
-				.name(LECTURE_NAME)
-				.generation(LECTURE_GENERATION)
-				.part(LECTURE_PART)
-				.attribute(LECTURE_ATTRIBUTE)
-				.subLectures(subLectures())
-				.attendances(attendancesStatus())
-				.status(LECTURE_STATUS)
-				.build();
-	}
-
-	private AttendancesStatusVO attendancesStatus() {
-		return AttendancesStatusVO.builder()
-				.attendance(80)
-				.absent(0)
-				.tardy(10)
-				.unknown(10)
-				.build();
-	}
-
-	private List<SubLectureVO> subLectures() {
-		return Stream.iterate(1, i -> i + 1).limit(LIST_SIZE)
-				.map(this::subLecture).toList();
-	}
-
-	private SubLectureVO subLecture(int i) {
-		return SubLectureVO.builder()
-				.subLectureId((long)i)
-				.round(i % 2)
-				.startAt(NOW.toString())
-				.code(SUB_LECTURE_CODE)
-				.build();
-	}
-
-	private AttendanceResponseDTO attendanceResponse() {
-		return new AttendanceResponseDTO(0L, 0L);
-	}
-
-	private AttendanceRequestDTO attendanceRequest() {
-		return new AttendanceRequestDTO(0L, 0, SUB_LECTURE_CODE);
-	}
-
-	private LectureDetailResponseDTO lectureDetail() {
-		return LectureDetailResponseDTO.builder()
-				.lectureId(0L)
-				.part(LECTURE_PART.getName())
-				.name(LECTURE_NAME)
-				.place(LECTURE_PLACE)
-				.attribute(LECTURE_ATTRIBUTE.getName())
-				.startDate(NOW.toString())
-				.endDate(NOW.plusHours(4).toString())
-				.generation(LECTURE_GENERATION)
 				.build();
 	}
 }
