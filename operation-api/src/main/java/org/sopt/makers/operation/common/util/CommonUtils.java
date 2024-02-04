3e12d516f98e64fa028b690173fd73d51f0fdf31
thguss <thgus345@naver.com>
2024-02-05 01:38:12 +0900

    [DELETE] 메소드 정리

--- operation-api/src/main/java/org/sopt/makers/operation/common/util/CommonUtils.java
+++ operation-api/src/main/java/org/sopt/makers/operation/common/util/CommonUtils.java
@@ -1,10 +1,8 @@
 package org.sopt.makers.operation.common.util;
 
-import java.net.URI;
 import java.security.Principal;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import lombok.NonNull;
 
@@ -12,13 +10,5 @@ import lombok.NonNull;
 public class CommonUtils {
 	public long getMemberId(@NonNull Principal principal) {
 		return Long.parseLong(principal.getName());
-	}
-
-	public URI getURI(String path, long id) {
-		return ServletUriComponentsBuilder
-				.fromCurrentRequest()
-				.path(path)
-				.buildAndExpand(id)
-				.toUri();
 	}
 }
