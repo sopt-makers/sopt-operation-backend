049da3b464e83f594404d041360644df091698a1
thguss <thgus345@naver.com>
2024-02-05 13:57:33 +0900

    [FIX] DTO 클래스 정리

--- operation-api/src/main/java/org/sopt/makers/operation/web/attendnace/service/AttendanceService.java
+++ operation-api/src/main/java/org/sopt/makers/operation/web/attendnace/service/AttendanceService.java
@@ -1,15 +1,15 @@
 package org.sopt.makers.operation.web.attendnace.service;
 
 import org.sopt.makers.operation.domain.Part;
-import org.sopt.makers.operation.web.attendance.dto.request.SubAttendanceUpdateRequest;
-import org.sopt.makers.operation.web.attendance.dto.response.AttendanceMemberResponse;
-import org.sopt.makers.operation.web.attendance.dto.response.AttendancesResponse;
-import org.sopt.makers.operation.web.attendance.dto.response.SubAttendanceUpdateResponse;
+import org.sopt.makers.operation.web.attendnace.dto.request.UpdatedSubAttendanceRequest;
+import org.sopt.makers.operation.web.attendnace.dto.response.AttendanceListResponse;
+import org.sopt.makers.operation.web.attendnace.dto.response.AttendanceMemberResponse;
+import org.sopt.makers.operation.web.attendnace.dto.response.UpdatedSubAttendanceResponse;
 import org.springframework.data.domain.Pageable;
 
 public interface AttendanceService {
-	SubAttendanceUpdateResponse updateSubAttendance(SubAttendanceUpdateRequest request);
+	UpdatedSubAttendanceResponse updateSubAttendance(UpdatedSubAttendanceRequest request);
 	AttendanceMemberResponse findAttendancesByMember(long memberId);
 	float updateMemberScore(Long memberId);
-	AttendancesResponse findAttendancesByLecture(long lectureId, Part part, Pageable pageable);
+	AttendanceListResponse findAttendancesByLecture(long lectureId, Part part, Pageable pageable);
 }
