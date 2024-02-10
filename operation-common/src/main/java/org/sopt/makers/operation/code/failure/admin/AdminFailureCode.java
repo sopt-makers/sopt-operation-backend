a60123a8ab96c8b538e1b794b9fcdaaca11aa244
dragontaek-lee <dragontaek98@naver.com>
2024-02-10 22:53:47 +0900

    [FIX] 리프레시 에러 메세지 수정

--- operation-common/src/main/java/org/sopt/makers/operation/code/failure/admin/AdminFailureCode.java
+++ operation-common/src/main/java/org/sopt/makers/operation/code/failure/admin/AdminFailureCode.java
@@ -14,7 +14,7 @@ public enum AdminFailureCode implements FailureCode {
     INVALID_EMAIL(BAD_REQUEST,"이메일이 존재하지 않습니다."),
     INVALID_PASSWORD(BAD_REQUEST,"비밀번호가 일치하지 않습니다."),
     NOT_APPROVED_ACCOUNT(BAD_REQUEST,"승인되지 않은 계정입니다."),
-    INVALID_TOKEN(BAD_REQUEST,"토큰이 일치하지 않습니다."),
+    INVALID_REFRESH_TOKEN(BAD_REQUEST,"유효하지 않은 리프레시 토큰입니다."),
     ;
 
     private final HttpStatus status;
