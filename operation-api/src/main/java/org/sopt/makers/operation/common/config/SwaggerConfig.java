6baa62be08991c0939a38e81c0bbaab4b195a246
thguss <thgus345@naver.com>
2024-02-10 20:56:44 +0900

    [FIX] 스웨거 변경

--- operation-api/src/main/java/org/sopt/makers/operation/common/config/SwaggerConfig.java
+++ operation-api/src/main/java/org/sopt/makers/operation/common/config/SwaggerConfig.java
@@ -1,75 +1,29 @@
 package org.sopt.makers.operation.common.config;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+@SecurityScheme(
+		name = "Authorization",
+		type = SecuritySchemeType.HTTP,
+		in = SecuritySchemeIn.HEADER,
+		bearerFormat = "JWT",
+		scheme = "Bearer"
+)
 @Configuration
-@EnableSwagger2
-@EnableWebMvc
 public class SwaggerConfig {
-
-	private ApiInfo swaggerInfo() {
-		return new ApiInfoBuilder().title("Makers Operation API")
-				.description("Makers Operation API Docs").build();
-	}
-
 	@Bean
-	public Docket swaggerApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.securityContexts(List.of(securityContext()))
-				.securitySchemes(List.of(apiKey()))
-				.consumes(getConsumeContentTypes())
-				.produces(getProduceContentTypes())
-				.apiInfo(swaggerInfo()).select()
-				.apis(RequestHandlerSelectors.basePackage("org.sopt.makers.operation.controller"))
-				.paths(PathSelectors.any())
-				.build()
-				.useDefaultResponseMessages(false);
-	}
-
-	private Set<String> getConsumeContentTypes() {
-		Set<String> consumes = new HashSet<>();
-		consumes.add("application/json");
-		return consumes;
-	}
-
-	private Set<String> getProduceContentTypes() {
-		Set<String> produces = new HashSet<>();
-		produces.add("application/json");
-		return produces;
-	}
-
-	private SecurityContext securityContext() {
-		return SecurityContext.builder()
-				.securityReferences(defaultAuth())
-				.build();
-	}
-
-	private List<SecurityReference> defaultAuth() {
-		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-		authorizationScopes[0] = authorizationScope;
-		return List.of(new SecurityReference("Authorization", authorizationScopes));
-	}
-
-	private ApiKey apiKey() {
-		return new ApiKey("Authorization", "Authorization", "header");
+	public OpenAPI api() {
+		Info info = new Info()
+				.title("Doorip API Docs")
+				.version("v1.0")
+				.description("Doorip 서비스 API 명세서 입니다.");
+		return new OpenAPI()
+				.info(info);
 	}
 }
