743621e7ad7cd2b3ac398efe1f6e796448854aeb
dragontaek-lee <dragontaek98@naver.com>
2024-02-18 23:54:43 +0900

    [FIX] admin builder 복구

--- operation-domain/src/main/java/org/sopt/makers/operation/admin/domain/Admin.java
+++ operation-domain/src/main/java/org/sopt/makers/operation/admin/domain/Admin.java
@@ -11,9 +11,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.sopt.makers.operation.exception.AdminFailureException;
-
-import static org.sopt.makers.operation.code.failure.admin.AdminFailureCode.INVALID_REFRESH_TOKEN;
 
 @Getter
 @Setter
@@ -42,6 +39,15 @@ public class Admin {
 
     @Column(name="refresh_token")
     private String refreshToken;
+
+    @Builder
+    public Admin(String email, String password, String name, Role role) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.role = role;
+        this.status = AdminStatus.NOT_CERTIFIED;
+    }
 
     public void updateRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
