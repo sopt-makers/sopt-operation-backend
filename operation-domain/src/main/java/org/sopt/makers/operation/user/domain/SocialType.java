a12ef89eafc7a08bf837b8570a2696673b695cf1
KWY <wodrk1001@gmail.com>
2024-05-30 00:03:25 +0900

    #T-10855 [feat] social type에 존재 여부 확인 기능 구현
    APPLE 또는 GOOGLE 외의 값이 들어올 때 false를 반환한다

--- operation-domain/src/main/java/org/sopt/makers/operation/user/domain/SocialType.java
+++ operation-domain/src/main/java/org/sopt/makers/operation/user/domain/SocialType.java
@@ -1,6 +1,15 @@
 package org.sopt.makers.operation.user.domain;
 
+import java.util.Arrays;
+
 public enum SocialType {
     GOOGLE,
-    APPLE,
+    APPLE;
+
+
+    public static boolean isContains(String type) {
+        SocialType[] socialTypes = SocialType.values();
+        return Arrays.stream(socialTypes)
+                .anyMatch(socialType -> socialType.name().equals(type));
+    }
 }
