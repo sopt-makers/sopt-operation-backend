c8264627d1f381d459d7413d114e64e024e1c0f0
KWY <wodrk1001@gmail.com>
2024-05-30 00:23:09 +0900

    #T-10855 [feat] /authorize controller 비즈니스 로직 구현
    1. query parameter로 들어오는 값이 null 인지 확인한다.
    2. 입력한 client id와 redirect uri가 존재하는지 확인한다.
    3. 입력한 type 값이 유효한지 확인한다.
    4. social type 과 code를 이용하여 social login 이후 user id를 불러온다.
    5. user id 기반으로 platform 인가코드를 생성하고 반환한다.

---
+++ operation-api/src/main/java/org/sopt/makers/operation/auth/api/AuthApiController.java
@@ -0,0 +1,66 @@
+package org.sopt.makers.operation.auth.api;
+
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.sopt.makers.operation.auth.dto.response.AuthorizationCodeResponse;
+import org.sopt.makers.operation.auth.service.AuthService;
+import org.sopt.makers.operation.dto.BaseResponse;
+import org.sopt.makers.operation.exception.AuthException;
+import org.sopt.makers.operation.user.domain.SocialType;
+import org.sopt.makers.operation.util.ApiResponseUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.sopt.makers.operation.code.failure.auth.AuthFailureCode.INVALID_SOCIAL_TYPE;
+import static org.sopt.makers.operation.code.failure.auth.AuthFailureCode.NOT_NULL_PARAMS;
+import static org.sopt.makers.operation.code.failure.auth.AuthFailureCode.NOT_FOUNT_REGISTERED_TEAM;
+import static org.sopt.makers.operation.code.success.auth.AuthSuccessCode.SUCCESS_GET_AUTHORIZATION_CODE;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class AuthApiController implements AuthApi {
+    private final ConcurrentHashMap<String, String> tempPlatformCode = new ConcurrentHashMap<>();
+    private final AuthService authService;
+
+    @Override
+    @GetMapping("/authorize")
+    public ResponseEntity<BaseResponse<?>> authorize(
+            @RequestParam String type,
+            @RequestParam String code,
+            @RequestParam String clientId,
+            @RequestParam String redirectUri
+    ) {
+        if (checkParamsIsNull(type, code, clientId, redirectUri)) throw new AuthException(NOT_NULL_PARAMS);
+        if (authService.checkRegisteredTeamOAuthInfo(clientId, redirectUri)) throw new AuthException(NOT_FOUNT_REGISTERED_TEAM);
+        if (!SocialType.isContains(type)) throw new AuthException(INVALID_SOCIAL_TYPE);
+
+        val userId = findUserIdBySocialTypeAndCode(type, code);
+        val platformCode = generatePlatformCode(userId);
+        return ApiResponseUtil.success(SUCCESS_GET_AUTHORIZATION_CODE, new AuthorizationCodeResponse(platformCode));
+    }
+
+    private boolean checkParamsIsNull(String... params) {
+        for (String param : params) {
+            if (param == null) return true;
+        }
+        return false;
+    }
+
+    private Long findUserIdBySocialTypeAndCode(String type, String code) {
+        val socialType = SocialType.valueOf(type);
+        val userSocialId = authService.getSocialUserInfo(socialType, code);
+        return authService.getUserId(socialType, userSocialId);
+    }
+
+    private String generatePlatformCode(Long userId) {
+        val platformCode = authService.generatePlatformCode(userId);
+        tempPlatformCode.putIfAbsent(platformCode, platformCode);
+        return platformCode;
+    }
+}
