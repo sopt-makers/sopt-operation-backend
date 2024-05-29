69d84e8774c65457e98da2b6d122202f8aa4ebaa
KWY <wodrk1001@gmail.com>
2024-05-29 23:53:02 +0900

    #T-10855 [feat] 인가 코드 발급 시 사용되는 상수 정의
    인가 코드 발급시 사용되는
    성공 메시지, 실패 메시지, 에러 model 구현 및 정의

---
+++ operation-common/src/main/java/org/sopt/makers/operation/code/success/auth/AuthSuccessCode.java
@@ -0,0 +1,16 @@
+package org.sopt.makers.operation.code.success.auth;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.sopt.makers.operation.code.success.SuccessCode;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthSuccessCode implements SuccessCode {
+    SUCCESS_GET_AUTHORIZATION_CODE(OK, "플랫폼 인가코드 발급 성공");
+    private final HttpStatus status;
+    private final String message;
+}
