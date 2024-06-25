82f102a4e26ff5c26587dd22c8677f596d6efc40
yummygyudon <bang2brew@gmail.com>
2024-06-25 17:20:40 +0900

    FEAT(`api`) : Encoded Request Query Parameter 변환 기능 구현 & Decoding 시 발생 가능한 Exception Customizing
    - 쉼표(`,`)를 통해 여러 User ID 값을 전달받는 Query Param 값을 List로 변환하는 기능입니다.
    - Java URL Encode/Decode 메서드 제공 라이브러리에서 발생할 수 있는 `UnsupportedEncodingException`에 대해 서비스 Exception(`ParameterDecodeCustomException`)으로 변환하도록 했습니다.

--- operation-api/src/main/java/org/sopt/makers/operation/common/util/CommonUtils.java
+++ operation-api/src/main/java/org/sopt/makers/operation/common/util/CommonUtils.java
@@ -1,14 +1,35 @@
 package org.sopt.makers.operation.common.util;
 
+import java.util.List;
+import java.util.Arrays;
+import java.net.URLEncoder;
 import java.security.Principal;
+import java.io.UnsupportedEncodingException;
 
+import org.sopt.makers.operation.code.failure.UserFailureCode;
+import org.sopt.makers.operation.exception.ParameterDecodeCustomException;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.NonNull;
 
 @Configuration
 public class CommonUtils {
+
+	private static final String DECODING_CHARSET = "UTF-8";
+	private static final String DELIMITER_ID_PARAMETER = ",";
+
 	public long getMemberId(@NonNull Principal principal) {
 		return Long.parseLong(principal.getName());
+	}
+
+	public List<Long> getIdsFromParameter(@NonNull String parameter)  {
+		try {
+			String encodedParameter = URLEncoder.encode(parameter, DECODING_CHARSET);
+			return Arrays.stream(encodedParameter.split(DELIMITER_ID_PARAMETER))
+					.map(id -> Long.parseLong(id.trim()))
+					.toList();
+		} catch (UnsupportedEncodingException ex) {
+			throw new ParameterDecodeCustomException(UserFailureCode.INVALID_PARAMETER, parameter);
+		}
 	}
 }
