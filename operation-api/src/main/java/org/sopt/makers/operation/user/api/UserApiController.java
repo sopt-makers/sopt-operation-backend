3c67692cd099b2904aabffd0d296f2d269082fbd
yummygyudon <bang2brew@gmail.com>
2024-06-26 18:19:42 +0900

    MODIFY(`api`) : CommonUtils 내 URL Decoding 메서드 이관

--- operation-api/src/main/java/org/sopt/makers/operation/user/api/UserApiController.java
+++ operation-api/src/main/java/org/sopt/makers/operation/user/api/UserApiController.java
@@ -4,8 +4,10 @@ import lombok.val;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+import org.sopt.makers.operation.code.failure.UserFailureCode;
 import org.sopt.makers.operation.common.util.CommonUtils;
 import org.sopt.makers.operation.dto.BaseResponse;
+import org.sopt.makers.operation.exception.ParameterDecodeCustomException;
 import org.sopt.makers.operation.user.service.UserService;
 import org.sopt.makers.operation.util.ApiResponseUtil;
 
@@ -15,6 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.Arrays;
+import java.util.List;
+import java.net.URLEncoder;
+import java.io.UnsupportedEncodingException;
 
 import static org.sopt.makers.operation.code.success.UserSuccessCode.SUCCESS_GET_USER;
 import static org.sopt.makers.operation.code.success.UserSuccessCode.SUCCESS_GET_USERS;
@@ -24,6 +30,9 @@ import static org.sopt.makers.operation.code.success.UserSuccessCode.SUCCESS_GET
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/internal/user")
 public class UserApiController implements UserApi {
+
+	private static final String DECODING_CHARSET = "UTF-8";
+	private static final String DELIMITER_ID_PARAMETER = ",";
 
 	private final UserService userService;
 	private final CommonUtils utils;
@@ -43,10 +52,20 @@ public class UserApiController implements UserApi {
 			@NonNull Principal principal,
 			@NonNull String targetUserIds
 	) {
-		val userIds = utils.getIdsFromParameter(targetUserIds);
+		val userIds = getIdsFromParameter(targetUserIds);
 		val response = userService.getUserInfos(userIds);
 		return ApiResponseUtil.success(SUCCESS_GET_USERS, response);
 	}
 
+	private List<Long> getIdsFromParameter(@NonNull String parameter)  {
+		try {
+			String encodedParameter = URLEncoder.encode(parameter, DECODING_CHARSET);
+			return Arrays.stream(encodedParameter.split(DELIMITER_ID_PARAMETER))
+					.map(id -> Long.parseLong(id.trim()))
+					.toList();
+		} catch (UnsupportedEncodingException ex) {
+			throw new ParameterDecodeCustomException(UserFailureCode.INVALID_PARAMETER, parameter);
+		}
+	}
 
 }
