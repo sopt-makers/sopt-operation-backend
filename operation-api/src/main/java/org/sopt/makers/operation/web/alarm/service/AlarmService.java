3507f6e017d1b99860625d8baac923f316236f36
yeseul106 <20191037@sungshin.ac.kr>
2024-06-27 13:50:31 +0900

    #255 [FEAT] 알림 즉시 발송/ 예약 발송 관련 Service단 로직 구현

--- operation-api/src/main/java/org/sopt/makers/operation/web/alarm/service/AlarmService.java
+++ operation-api/src/main/java/org/sopt/makers/operation/web/alarm/service/AlarmService.java
@@ -1,18 +1,20 @@
 package org.sopt.makers.operation.web.alarm.service;
 
-import org.sopt.makers.operation.common.domain.Part;
 import org.sopt.makers.operation.alarm.domain.Status;
-import org.sopt.makers.operation.web.alarm.dto.request.AlarmCreateRequest;
-import org.sopt.makers.operation.web.alarm.dto.request.AlarmSendRequest;
-import org.sopt.makers.operation.web.alarm.dto.response.AlarmCreateResponse;
+import org.sopt.makers.operation.web.alarm.dto.request.AlarmInstantSendRequest;
+import org.sopt.makers.operation.web.alarm.dto.request.AlarmScheduleSendRequest;
 import org.sopt.makers.operation.web.alarm.dto.response.AlarmGetResponse;
 import org.sopt.makers.operation.web.alarm.dto.response.AlarmListGetResponse;
 import org.springframework.data.domain.Pageable;
 
 public interface AlarmService {
-	void sendAlarm(AlarmSendRequest request);
-	AlarmCreateResponse saveAlarm(AlarmCreateRequest requestDTO);
-	AlarmListGetResponse getAlarms(Integer generation, Part part, Status status, Pageable pageable);
-	AlarmGetResponse getAlarm(long alarmId);
-	void deleteAlarm(long alarmId);
+    void sendAlarm(AlarmInstantSendRequest requestDTO);
+
+    AlarmListGetResponse getAlarms(Integer generation, Status status, Pageable pageable);
+
+    AlarmGetResponse getAlarm(long alarmId);
+
+    void deleteAlarm(long alarmId);
+
+    void scheduleAlarm(AlarmScheduleSendRequest request);
 }
