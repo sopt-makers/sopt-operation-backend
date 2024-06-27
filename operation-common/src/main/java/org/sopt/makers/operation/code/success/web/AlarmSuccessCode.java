549a2de483aec417be34823b2fc62ced434b2ee4
yeseul106 <20191037@sungshin.ac.kr>
2024-06-27 13:40:06 +0900

    #255 [CHORE] 알림 성공 메세지 관련 enum 재정의

--- operation-common/src/main/java/org/sopt/makers/operation/code/success/web/AlarmSuccessCode.java
+++ operation-common/src/main/java/org/sopt/makers/operation/code/success/web/AlarmSuccessCode.java
@@ -1,23 +1,21 @@
 package org.sopt.makers.operation.code.success.web;
 
-import static org.springframework.http.HttpStatus.*;
-
-import org.sopt.makers.operation.code.success.SuccessCode;
-import org.springframework.http.HttpStatus;
+import static org.springframework.http.HttpStatus.OK;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.sopt.makers.operation.code.success.SuccessCode;
+import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 @Getter
 public enum AlarmSuccessCode implements SuccessCode {
-	SUCCESS_SEND_ALARM(OK, "알림 전송 성공"),
-	SUCCESS_CREATE_ALARM(CREATED, "알림 생성 성공"),
-	SUCCESS_GET_ALARMS(OK, "알림 리스트 조회 성공"),
-	SUCCESS_GET_ALARM(OK, "알림 상세 조회 성공"),
-	SUCCESS_DELETE_ALARM(OK, "알림 삭제 성공"),
-	;
+    SUCCESS_SEND_ALARM(OK, "알림 즉시 발송 성공"),
+    SUCCESS_GET_ALARMS(OK, "알림 리스트 조회 성공"),
+    SUCCESS_GET_ALARM(OK, "알림 상세 조회 성공"),
+    SUCCESS_DELETE_ALARM(OK, "알림 삭제 성공"),
+    SUCCESS_SCHEDULE_ALARM(OK, "알림 예약 발송 성공");
 
-	private final HttpStatus status;
-	private final String message;
+    private final HttpStatus status;
+    private final String message;
 }
