80541b0e11ebba19325ab6424ec5808bd4fc8dc6
yummygyudon <bang2brew@gmail.com>
2024-07-02 23:46:35 +0900

    FEAT(`api`) : 복수 유저 조회 기능 구현
    - User 리스트를 조회하여 map을 통해 UserGenerationHistory 리스트 조회 & DTO 변환했습니다.
    - `getUserInfo` 메서드의 파라미터에도 `@Min(1)` Valid 어노테이션을 추가했습니다.
    - 유효하지 않은 ID 포함 여부를 검사하는 메서드를 추가했습니다.
      - 파라미터로 주입된 id들 중 1보다 작은 경우

--- operation-api/src/main/java/org/sopt/makers/operation/user/service/UserServiceImpl.java
+++ operation-api/src/main/java/org/sopt/makers/operation/user/service/UserServiceImpl.java
@@ -5,7 +5,11 @@ import java.util.List;
 import lombok.val;
 import lombok.RequiredArgsConstructor;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.transaction.annotation.Transactional;
 
 import org.sopt.makers.operation.user.repository.UserRepository;
@@ -13,8 +17,12 @@ import org.sopt.makers.operation.user.repository.history.UserGenerationHistoryRe
 import org.sopt.makers.operation.user.dto.response.UserInfoResponse;
 import org.sopt.makers.operation.user.dto.response.UserInfosResponse;
 
+import org.sopt.makers.operation.code.failure.UserFailureCode;
+import org.sopt.makers.operation.exception.UserException;
+
 @Service
 @RequiredArgsConstructor
+@Validated
 @Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
 
@@ -22,14 +30,29 @@ public class UserServiceImpl implements UserService {
     private final UserGenerationHistoryRepository generationHistoryRepository;
 
     @Override
-    public UserInfoResponse getUserInfo(Long userId) {
+    public UserInfoResponse getUserInfo(@Min(1) Long userId) {
         val targetUser = userRepository.findUserById(userId);
         val histories = generationHistoryRepository.findAllHistoryByUserId(userId);
         return UserInfoResponse.of(targetUser, histories);
     }
 
     @Override
-    public UserInfosResponse getUserInfos(List<Long> userIds) {
-        return null;
+    public UserInfosResponse getUserInfos(@NotEmpty List<Long> userIds) {
+        validateIds(userIds);
+        val targetUsers = userRepository.findAllUsersById(userIds);
+        val userInfoResponses = targetUsers.stream()
+                .map(user -> {
+                    val histories = generationHistoryRepository.findAllHistoryByUserId(user.getId());
+                    return UserInfoResponse.of(user, histories);
+                }).toList();
+        return UserInfosResponse.of(userInfoResponses);
     }
+
+    private void validateIds(List<Long> ids) {
+        val isContainLittleThenOne  = ids.stream().anyMatch(id -> id < 1);
+        if (isContainLittleThenOne) {
+            throw new UserException(UserFailureCode.INVALID_USER_INCLUDED);
+        }
+    }
+
 }
