4e345c6bb7c151dd969e9f174b42d36a6a1aae1a
yeseul106 <20191037@sungshin.ac.kr>
2024-07-05 17:09:39 +0900

    #255 [REFACTOR] MethodArgumentNotValidException 핸들링 코드 리팩토링

--- operation-api/src/main/java/org/sopt/makers/operation/common/handler/ErrorHandler.java
+++ operation-api/src/main/java/org/sopt/makers/operation/common/handler/ErrorHandler.java
@@ -3,6 +3,7 @@ package org.sopt.makers.operation.common.handler;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.sopt.makers.operation.dto.BaseResponse;
 import org.sopt.makers.operation.exception.AdminFailureException;
 import org.sopt.makers.operation.exception.AlarmException;
@@ -17,6 +18,7 @@ import org.sopt.makers.operation.exception.TokenException;
 import org.sopt.makers.operation.util.ApiResponseUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -85,19 +87,24 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<BaseResponse<?>> methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<BaseResponse<?>> handleValidationException(MethodArgumentNotValidException ex) {
         List<String> errorMessages = ex.getBindingResult().getAllErrors().stream()
-                .map(objectError -> {
-                    if (objectError instanceof FieldError) {
-                        FieldError fieldError = (FieldError) objectError;
-                        return String.format("[%s]은 필수 값입니다.", fieldError.getField());
-                    } else {
-                        return objectError.getDefaultMessage();
-                    }
-                })
+                .map(this::getErrorMessage)
                 .collect(Collectors.toList());
-        log.error("[ValidationException] : {}", String.join(", ", errorMessages));
-        return ApiResponseUtil.failure(String.join(", ", errorMessages));
+
+        val errorMessage = String.join(", ", errorMessages);
+        log.error("[ValidationException] : {}", errorMessage);
+
+        return ApiResponseUtil.failure(errorMessage);
+    }
+
+    private String getErrorMessage(ObjectError objectError) {
+        if (objectError instanceof FieldError) {
+            FieldError fieldError = (FieldError) objectError;
+            return String.format("[%s]은 필수 값입니다.", fieldError.getField());
+        } else {
+            return objectError.getDefaultMessage();
+        }
     }
 
 }
