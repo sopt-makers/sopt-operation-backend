3f05e63875365d64d28ecf62464b10175b928bf5
yummygyudon <bang2brew@gmail.com>
2024-07-12 06:10:58 +0900

    FEAT(`api`) : 유저 인적 정보 Update 메서드 & 유저 활동 이력 Update 메서드 정의
    - Controller 단에서의 해당 API 에서 어떤 처리 대상에 대해서 어떤 행위를 하는지에 대한 내용을 전달하고자 메서드를 분리했습니다.

--- operation-api/src/main/java/org/sopt/makers/operation/user/service/UserService.java
+++ operation-api/src/main/java/org/sopt/makers/operation/user/service/UserService.java
@@ -2,10 +2,15 @@ package org.sopt.makers.operation.user.service;
 
 import java.util.List;
 
+import org.sopt.makers.operation.user.dto.request.UserActivityModifyRequest;
+import org.sopt.makers.operation.user.dto.request.UserModifyRequest;
 import org.sopt.makers.operation.user.dto.response.UserInfoResponse;
 import org.sopt.makers.operation.user.dto.response.UserInfosResponse;
 
 public interface UserService {
     UserInfoResponse getUserInfo(Long userId);
     UserInfosResponse getUserInfos(List<Long> userIds);
+
+    void modifyUserPersonalInfo(UserModifyRequest userModifyRequest);
+    void modifyUserActivityInfos(List<UserActivityModifyRequest> activitiesModifyRequests);
 }
