af94b6a2d309d3533c568818b84189c728a1864f
yummygyudon <bang2brew@gmail.com>
2024-07-12 05:31:54 +0900

    FEAT(`common`) : User 정보 수정 PUT API에 대한 SuccessCode 구현
    - 수정 성공 시, 204(NO CONTENT) Status Code를 반환합니다.
      - 요청 값에 따라 성공적으로 수정된 경우, 서버는 요청이 성공적으로 완료되었다는 의미로 200( OK) 또는 204(NO CONTENT) 응답을 하도록 합니다.
      - [참고 링크](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/PUT)

--- operation-common/src/main/java/org/sopt/makers/operation/code/success/UserSuccessCode.java
+++ operation-common/src/main/java/org/sopt/makers/operation/code/success/UserSuccessCode.java
@@ -5,12 +5,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @Getter
 @RequiredArgsConstructor
 public enum UserSuccessCode implements SuccessCode {
+
+    // 200
     SUCCESS_GET_USER(OK, "유저 정보 조회 성공"),
     SUCCESS_GET_USERS(OK, "전체 유저 정보 조회 성공"),
+
+    // 204
+    SUCCESS_PUT_USER(NO_CONTENT, "유저 정보 수정 성공")
     ;
 
     private final HttpStatus status;
