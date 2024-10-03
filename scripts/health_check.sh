a1a6e9d4d9f2dd0b70058b4e49874e14e1c42f9a
yummygyudon <bang2brew@gmail.com>
2024-10-04 03:07:48 +0900

    [CHORE] 비교문 String 비교로 변경

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 echo "> Health Check Start"
+
+
 CURRENT_RUNNING_PORT=$(cat /etc/nginx/conf.d/service-url.inc | grep -Po '[0-9]+' | tail -1)
 echo "> Current port of running WAS is ${CURRENT_RUNNING_PORT}."
 
-if [ ${CURRENT_RUNNING_PORT} -ne 8081 ]; then
+if [ "${CURRENT_RUNNING_PORT}" -ne "8081" ]; then
     # run_new_was 에서 정상적으로 처리되지 않았다는 의미 - 더 이상 진행되면 안된다.
     echo "> No WAS is connected to nginx"
     exit 1
-elif [ ${CURRENT_RUNNING_PORT} -ne 8082 ]; then
+elif [ "${CURRENT_RUNNING_PORT}" -ne "8082" ]; then
     echo "> No WAS is connected to nginx"
     exit 1
 fi
