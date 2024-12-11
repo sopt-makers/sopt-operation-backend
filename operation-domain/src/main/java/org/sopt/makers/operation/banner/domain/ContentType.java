e198d1c485789ebfc81d04e26e5487916a6bc99a
yummygyudon <bang2brew@gmail.com>
2024-12-12 03:45:31 +0900

    [FEAT] Banner 구성 도메인 요소 Enum 구현
    소문자인 외부 반환 데이터를 관리하는 필드 `value`을 선언했습니다.
    또한 외부 조회 등에 사용될 경우, `value`를 통해 조회할 수 있도록 하기 위해 `getByValue` 메서드를 정의했고 조회하려는 값에 해당하는 Enum이 없을 경우 Banner Exception이 발생하도록 했습니다.
    - ContentType :  게시물 유형
    - PublishLocation : 게시 위치
    - PublishStatus : 게시 상태

---
+++ operation-domain/src/main/java/org/sopt/makers/operation/banner/domain/ContentType.java
@@ -0,0 +1,30 @@
+package org.sopt.makers.operation.banner.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.sopt.makers.operation.code.failure.BannerFailureCode;
+import org.sopt.makers.operation.exception.BannerException;
+
+import java.util.Arrays;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Getter
+@RequiredArgsConstructor(access = PRIVATE)
+public enum ContentType {
+    PRODUCT("product"),
+    BIRTHDAY("birthday"),
+    SPONSOR("sponsor"),
+    EVENT("event"),
+    ETC("etc"),
+    ;
+
+    private final String value;
+
+    public static PublishLocation getByValue(String value) {
+        return Arrays.stream(PublishLocation.values())
+                .filter(location -> location.getValue().equals(value))
+                .findAny().orElseThrow(() -> new BannerException(BannerFailureCode.NOT_FOUND_CONTENT_TYPE));
+    }
+}
