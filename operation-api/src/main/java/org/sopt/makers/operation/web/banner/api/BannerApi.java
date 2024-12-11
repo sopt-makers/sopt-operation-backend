a2459f035f1b7afc422a6b376ac601ad5606a98f
yummygyudon <bang2brew@gmail.com>
2024-12-11 11:14:00 +0900

    [FEAT] Banner 상세 조회 API 정의
    - 조회 성공 상태에 대한 Success Status Code Enum 추가 구현했습니다. (`BannerSuccessCode`)

---
+++ operation-api/src/main/java/org/sopt/makers/operation/web/banner/api/BannerApi.java
@@ -0,0 +1,34 @@
+package org.sopt.makers.operation.web.banner.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import org.sopt.makers.operation.dto.BaseResponse;
+
+import org.springframework.http.ResponseEntity;
+
+public interface BannerApi {
+    @Operation(
+            summary = "배너 상세 조회 API",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "배너 상세 조회 성공"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "잘못된 요청"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "존재하지 않는 배너 ID 요청"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "서버 내부 오류"
+                    )
+            }
+    )
+    ResponseEntity<BaseResponse<?>> getBannerDetail(Long bannerId);
+
+}
---
+++ operation-common/src/main/java/org/sopt/makers/operation/code/success/web/BannerSuccessCode.java
@@ -0,0 +1,19 @@
+package org.sopt.makers.operation.code.success.web;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.sopt.makers.operation.code.success.SuccessCode;
+import org.springframework.http.HttpStatus;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Getter
+@RequiredArgsConstructor(access = PRIVATE)
+public enum BannerSuccessCode implements SuccessCode {
+    SUCCESS_GET_BANNER_DETAIL(HttpStatus.OK, "배너 상세 정보 조회 성공"),
+
+    ;
+    private final HttpStatus status;
+    private final String message;
+}
