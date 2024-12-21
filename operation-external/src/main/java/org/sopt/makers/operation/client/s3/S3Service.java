38d8dd810808b4499c81291a1b023d479ff0d254
sung-silver <jinna0319@gmail.com>
2024-12-21 17:13:20 +0900

    [FEAT] S3Service에 createPutPreSignedUrl 메서드 정의

---
+++ operation-external/src/main/java/org/sopt/makers/operation/client/s3/S3Service.java
@@ -0,0 +1,5 @@
+package org.sopt.makers.operation.client.s3;
+
+public interface S3Service {
+    String createPutPreSignedUrl(String fileName);
+}
---
+++ operation-external/src/main/java/org/sopt/makers/operation/client/s3/S3ServiceImpl.java
@@ -0,0 +1,8 @@
+package org.sopt.makers.operation.client.s3;
+
+public class S3ServiceImpl implements S3Service {
+    @Override
+    public String createPutPreSignedUrl(String fileName) {
+        return null;
+    }
+}
