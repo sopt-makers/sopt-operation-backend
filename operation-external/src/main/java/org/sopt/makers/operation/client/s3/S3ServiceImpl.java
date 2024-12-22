db292596b048df49e9a0f9875375ba848f526488
sung-silver <jinna0319@gmail.com>
2024-12-22 22:57:18 +0900

    [FEAT] s3ServiceImpl에 createPutPreSignedUrl 구현

--- operation-external/src/main/java/org/sopt/makers/operation/client/s3/S3ServiceImpl.java
+++ operation-external/src/main/java/org/sopt/makers/operation/client/s3/S3ServiceImpl.java
@@ -1,8 +1,29 @@
 package org.sopt.makers.operation.client.s3;
 
+import java.time.Duration;
+
+import lombok.RequiredArgsConstructor;
+
+import org.sopt.makers.operation.config.*;
+import org.springframework.stereotype.Service;
+
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+
+@Service
+@RequiredArgsConstructor
 public class S3ServiceImpl implements S3Service {
+    private final S3Presigner s3Presigner;
+    private static final int SIGNATURE_DURATION = 20;
+
     @Override
-    public String createPutPreSignedUrl(String fileName) {
-        return null;
+    public String createPutPreSignedUrl(String bucketName, String fileName) {
+        PresignedPutObjectRequest preSignedRequest = s3Presigner
+                .presignPutObject(r -> r.signatureDuration(Duration.ofMinutes(SIGNATURE_DURATION))
+                        .putObjectRequest(por -> por.bucket(bucketName)
+                                .key(fileName)
+                                .acl(ObjectCannedACL.PUBLIC_READ)));
+        return preSignedRequest.url().toString();
     }
 }
