a8b63afbd2af7a82c8c1fb1f4fbc460127a97290
sung-silver <jinna0319@gmail.com>
2024-12-23 03:02:39 +0900

    [FEAT] 배너이미지 클래스에 대해 빌더패턴 정의

--- operation-domain/src/main/java/org/sopt/makers/operation/banner/domain/BannerImage.java
+++ operation-domain/src/main/java/org/sopt/makers/operation/banner/domain/BannerImage.java
@@ -2,9 +2,7 @@ package org.sopt.makers.operation.banner.domain;
 
 import jakarta.persistence.Embeddable;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import static lombok.AccessLevel.PROTECTED;
 
@@ -22,6 +20,12 @@ public class BannerImage {
 
     public void updateMobileImage(String updateMobileImageUrl) {
         this.mobileImageUrl = updateMobileImageUrl;
+    }
+
+    @Builder
+    private BannerImage(String pcImageUrl, String mobileImageUrl) {
+        this.pcImageUrl = pcImageUrl;
+        this.mobileImageUrl = mobileImageUrl;
     }
 
 }
