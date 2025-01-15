9533aaf2df3261e81b79a01f2aebb6fed47ffaff
yummygyudon <bang2brew@gmail.com>
2025-01-15 18:45:27 +0900

    [REFACTOR] 즉시/예약 발송 알림 생성 요청 DTO 내 개편 Domain Entity 반영

--- operation-api/src/main/java/org/sopt/makers/operation/web/alarm/dto/request/AlarmInstantSendRequest.java
+++ operation-api/src/main/java/org/sopt/makers/operation/web/alarm/dto/request/AlarmInstantSendRequest.java
@@ -2,39 +2,47 @@ package org.sopt.makers.operation.web.alarm.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
-import org.sopt.makers.operation.alarm.domain.Alarm;
-import org.sopt.makers.operation.alarm.domain.AlarmType;
-import org.sopt.makers.operation.alarm.domain.Category;
-import org.sopt.makers.operation.alarm.domain.LinkType;
-import org.sopt.makers.operation.alarm.domain.Status;
-import org.sopt.makers.operation.alarm.domain.TargetType;
-import org.sopt.makers.operation.common.domain.Part;
+
+import org.sopt.makers.operation.alarm.domain.*;
+import org.sopt.makers.operation.exception.AlarmException;
+
+import static org.sopt.makers.operation.code.failure.AlarmFailureCode.INVALID_SEND_INSTANT_REQUEST;
 
 public record AlarmInstantSendRequest(
-        @NotNull Integer createdGeneration,
         @NotNull String title,
         @NotNull String content,
-        @NotNull Category category,
-        @NotNull TargetType targetType,
+        @NotNull AlarmCategory category,
+        @NotNull AlarmTargetType targetType,
+        AlarmTargetPart part,
+        Integer createdGeneration,
         List<String> targetList,
-        Part part,
-        LinkType linkType,
+        AlarmLinkType linkType,
         String link
 ) {
 
+    private AlarmTarget toTargetEntity() {
+        return switch (this.targetType) {
+            case ALL -> this.part.equals(AlarmTargetPart.ALL)
+                            ? AlarmTarget.all()
+                            : AlarmTarget.partialForAll(this.part, this.targetList);
+            case ACTIVE -> AlarmTarget.partialForActive(this.createdGeneration, this.part, this.targetList);
+            case CSV -> AlarmTarget.partialForCsv(this.targetList);
+            default -> throw new AlarmException(INVALID_SEND_INSTANT_REQUEST);
+        };
+    }
+
+    private AlarmContent toContentEntity() {
+        return switch (this.linkType) {
+            case WEB -> AlarmContent.withWebLink(this.title, this.content, this.category, this.link);
+            case APP -> AlarmContent.withAppLink(this.title, this.content, this.category, this.link);
+            case NONE -> AlarmContent.withoutLink(this.title, this.content, this.category);
+            default -> throw new AlarmException(INVALID_SEND_INSTANT_REQUEST);
+        };
+    }
+
     public Alarm toEntity() {
-        return Alarm.builder()
-                .createdGeneration(this.createdGeneration)
-                .category(this.category)
-                .title(this.title)
-                .content(this.content)
-                .targetType(this.targetType)
-                .alarmType(AlarmType.INSTANT)
-                .link(this.link)
-                .linkType(linkType)
-                .part(this.part)
-                .status(Status.COMPLETED)
-                .targetList(this.targetList)
-                .build();
+        AlarmTarget targetEntity = this.toTargetEntity();
+        AlarmContent contentEntity = this.toContentEntity();
+        return Alarm.instant(targetEntity, contentEntity);
     }
 }
--- operation-api/src/main/java/org/sopt/makers/operation/web/alarm/dto/request/AlarmScheduleSendRequest.java
+++ operation-api/src/main/java/org/sopt/makers/operation/web/alarm/dto/request/AlarmScheduleSendRequest.java
@@ -1,43 +1,61 @@
 package org.sopt.makers.operation.web.alarm.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
-import org.sopt.makers.operation.alarm.domain.Alarm;
-import org.sopt.makers.operation.alarm.domain.AlarmType;
-import org.sopt.makers.operation.alarm.domain.Category;
-import org.sopt.makers.operation.alarm.domain.LinkType;
-import org.sopt.makers.operation.alarm.domain.Status;
-import org.sopt.makers.operation.alarm.domain.TargetType;
+
+import lombok.val;
+import org.sopt.makers.operation.alarm.domain.*;
+import org.sopt.makers.operation.code.failure.AlarmFailureCode;
 import org.sopt.makers.operation.common.domain.Part;
+import org.sopt.makers.operation.exception.AlarmException;
+
+import static org.sopt.makers.operation.code.failure.AlarmFailureCode.INVALID_SEND_SCHEDULED_REQUEST;
 
 
 public record AlarmScheduleSendRequest(
-        @NotNull Integer createdGeneration,
         @NotNull String title,
         @NotNull String content,
-        @NotNull Category category,
-        @NotNull TargetType targetType,
+        @NotNull AlarmCategory category,
+        @NotNull AlarmTargetType targetType,
         List<String> targetList,
-        Part part,
-        LinkType linkType,
+        AlarmTargetPart part,
+        Integer createdGeneration,
+        AlarmLinkType linkType,
         String link,
         @NotNull String postDate,
         @NotNull String postTime
 ) {
+    private AlarmTarget toTargetEntity() {
+        return switch (this.targetType) {
+            case ALL -> this.part.equals(AlarmTargetPart.ALL)
+                    ? AlarmTarget.all()
+                    : AlarmTarget.partialForAll(this.part, this.targetList);
+            case ACTIVE -> AlarmTarget.partialForActive(this.createdGeneration, this.part, this.targetList);
+            case CSV -> AlarmTarget.partialForCsv(this.targetList);
+            default -> throw new AlarmException(INVALID_SEND_SCHEDULED_REQUEST);
+        };
+    }
+
+    private AlarmContent toContentEntity() {
+        return switch (this.linkType) {
+            case WEB -> AlarmContent.withWebLink(this.title, this.content, this.category, this.link);
+            case APP -> AlarmContent.withAppLink(this.title, this.content, this.category, this.link);
+            case NONE -> AlarmContent.withoutLink(this.title, this.content, this.category);
+            default -> throw new AlarmException(INVALID_SEND_SCHEDULED_REQUEST);
+        };
+    }
+
     public Alarm toEntity() {
-        return Alarm.builder()
-                .createdGeneration(this.createdGeneration)
-                .category(this.category)
-                .title(this.title)
-                .content(this.content)
-                .targetType(this.targetType)
-                .alarmType(AlarmType.RESERVED)
-                .link(this.link)
-                .linkType(linkType)
-                .part(this.part)
-                .status(Status.SCHEDULED)
-                .targetList(this.targetList)
-                .build();
+        AlarmTarget targetEntity = this.toTargetEntity();
+        AlarmContent contentEntity = this.toContentEntity();
+        val date = LocalDate.parse(postDate, DateTimeFormatter.ISO_LOCAL_DATE);
+        val time = LocalTime.parse(postTime, DateTimeFormatter.ofPattern("HH:mm"));
+        return Alarm.scheduled(targetEntity, contentEntity, LocalDateTime.of(date, time));
     }
 }
 
