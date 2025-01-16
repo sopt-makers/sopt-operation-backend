befd8381690346782171674ba76e260e02ba9dcd
yummygyudon <bang2brew@gmail.com>
2025-01-17 02:11:04 +0900

    [REFACTOR] 예약 알림 발송 전용 EventBridge 구현체 개편
    EventBridge의 경우, 다양한 도메인에서 활용될 수 있기 때문에 이름에 쓰임과 의도를 명확히 표현하기 위해 이름을 수정했습니다.
    - 이전 구현체 대비 코드 절약이 가능한 소스 코드는 편집했습니다.

---
+++ operation-external/src/main/java/org/sopt/makers/operation/client/alarm/dto/eventbridge/AlarmScheduleEventBridgeBody.java
@@ -0,0 +1,18 @@
+package org.sopt.makers.operation.client.alarm.dto.eventbridge;
+
+import java.util.List;
+
+import lombok.Builder;
+import org.sopt.makers.operation.alarm.domain.AlarmCategory;
+
+@Builder
+public record AlarmScheduleEventBridgeBody(
+        long alarmId,
+        List<String> userIds,
+        String title,
+        String content,
+        AlarmCategory category,
+        String deepLink,
+        String webLink
+) {
+}
---
+++ operation-external/src/main/java/org/sopt/makers/operation/client/alarm/dto/eventbridge/AlarmScheduleEventBridgeHeader.java
@@ -0,0 +1,12 @@
+package org.sopt.makers.operation.client.alarm.dto.eventbridge;
+
+import lombok.Builder;
+
+@Builder
+public record AlarmScheduleEventBridgeHeader(
+        String action,
+        String xApiKey,
+        String transactionId,
+        String service
+) {
+}
