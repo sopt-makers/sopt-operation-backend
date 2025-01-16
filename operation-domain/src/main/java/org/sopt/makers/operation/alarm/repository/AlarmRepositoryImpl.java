d4d63a97be355dce89357f174041a83b1328afa3
yummygyudon <bang2brew@gmail.com>
2025-01-16 20:50:28 +0900

    [REFACTOR] Alarm Entity 개편에 따른 QueryDSL 소스 코드 변경

--- operation-domain/src/main/java/org/sopt/makers/operation/alarm/repository/AlarmRepositoryImpl.java
+++ operation-domain/src/main/java/org/sopt/makers/operation/alarm/repository/AlarmRepositoryImpl.java
@@ -8,7 +8,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.sopt.makers.operation.alarm.domain.Alarm;
-import org.sopt.makers.operation.alarm.domain.Status;
+import org.sopt.makers.operation.alarm.domain.AlarmStatus;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
@@ -18,7 +18,7 @@ public class AlarmRepositoryImpl implements AlarmCustomRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public List<Alarm> findOrderByCreatedDate(Integer generation, Status status, Pageable pageable) {
+    public List<Alarm> findOrderByCreatedDate(Integer generation, AlarmStatus status, Pageable pageable) {
         return queryFactory
                 .selectFrom(alarm)
                 .where(
@@ -32,7 +32,7 @@ public class AlarmRepositoryImpl implements AlarmCustomRepository {
     }
 
     @Override
-    public int count(Integer generation, Status status) {
+    public int count(Integer generation, AlarmStatus status) {
         return Math.toIntExact(queryFactory
                 .select(alarm.count())
                 .from(alarm)
@@ -44,10 +44,10 @@ public class AlarmRepositoryImpl implements AlarmCustomRepository {
     }
 
     private BooleanExpression generationEq(Integer generation) {
-        return nonNull(generation) ? alarm.createdGeneration.eq(generation) : null;
+        return nonNull(generation) ? alarm.target.generation.eq(generation) : null;
     }
 
-    private BooleanExpression statusEq(Status status) {
+    private BooleanExpression statusEq(AlarmStatus status) {
         return nonNull(status) ? alarm.status.eq(status) : null;
     }
 }
