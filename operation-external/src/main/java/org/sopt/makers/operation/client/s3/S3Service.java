a25d489d0d9c44b606958efdf9f84652f0060563
softmoca <acyc0909@gmail.com>
2025-03-24 14:54:18 +0900

    [FEAT] S3 이미지 업로드 기능 추가 및 파일 확장자/크기 검증 로직 구현

--- operation-external/src/main/java/org/sopt/makers/operation/client/s3/S3Service.java
+++ operation-external/src/main/java/org/sopt/makers/operation/client/s3/S3Service.java
@@ -1,9 +1,14 @@
 package org.sopt.makers.operation.client.s3;
 
+import java.io.IOException;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface S3Service {
     String createPreSignedUrlForPutObject(String bucketName, String fileName);
 
     String getUrl(String bucketName, String fileName);
 
     void deleteFile(String bucketName, String fileName);
+
+    String uploadImage(String directoryPath, MultipartFile image ,String bucketName) throws IOException;
 }
--- operation-external/src/main/java/org/sopt/makers/operation/client/s3/S3ServiceImpl.java
+++ operation-external/src/main/java/org/sopt/makers/operation/client/s3/S3ServiceImpl.java
@@ -2,14 +2,20 @@ package org.sopt.makers.operation.client.s3;
 
 import static org.sopt.makers.operation.code.failure.ExternalFailureCode.NOT_FOUND_S3_RESOURCE;
 
+import java.io.IOException;
 import java.time.Duration;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 import lombok.val;
 import lombok.RequiredArgsConstructor;
 
 import org.sopt.makers.operation.exception.*;
 import org.springframework.stereotype.Service;
 
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.*;
 import software.amazon.awssdk.services.s3.model.*;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -21,6 +27,8 @@ public class S3ServiceImpl implements S3Service {
     private final S3Presigner s3Presigner;
     private final S3Client s3Client;
     private static final int SIGNATURE_DURATION = 20;
+    private static final Long MAX_FILE_SIZE = 5 * 1024 * 1024L;
+    private static final List<String> IMAGE_EXTENSIONS = Arrays.asList("image/png");
 
     @Override
     public String createPreSignedUrlForPutObject(String bucketName, String fileName) {
@@ -50,4 +58,46 @@ public class S3ServiceImpl implements S3Service {
     public void deleteFile(String bucketName, String fileName) {
         s3Client.deleteObject(b -> b.bucket(bucketName).key(fileName));
     }
+
+
+    @Override
+    public String uploadImage(String directoryPath, MultipartFile image,String bucketName) throws IOException {
+        final String key = directoryPath + generateImageFileName();
+
+
+        validateExtension(image);
+        validateFileSize(image);
+
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .contentType(image.getContentType())
+                .contentDisposition("inline")
+                .build();
+
+        RequestBody requestBody = RequestBody.fromBytes(image.getBytes());
+        s3Client.putObject(request, requestBody);
+        return key;
+    }
+
+    private void validateExtension(MultipartFile image) {
+        String contentType = image.getContentType();
+        if (!IMAGE_EXTENSIONS.contains(contentType)) {
+            throw new RuntimeException("이미지 확장자는 png만 가능합니다.");
+        }
+    }
+
+    private String generateImageFileName() {
+        return UUID.randomUUID() + ".png";
+    }
+
+
+
+
+    private void validateFileSize(MultipartFile image) {
+        if (image.getSize() > MAX_FILE_SIZE) {
+            throw new RuntimeException("이미지 사이즈는 5MB를 넘을 수 없습니다.");
+        }
+    }
+
 }
