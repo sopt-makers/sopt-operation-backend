ce8e9f714a98d454a73a55ae4fa82cde72d41b4c
softmoca <acyc0909@gmail.com>
2025-03-24 14:52:23 +0900

    [CHORE] banners 테이블 컬럼명 변경으로 인한  insert.sql 및 테스트 코드 수정

--- operation-api/src/main/resources/data/insert-get-banners-data.sql
+++ operation-api/src/main/resources/data/insert-get-banners-data.sql
@@ -3,8 +3,8 @@ CREATE TABLE if not exists banners (
      created_date timestamp(6),
      last_modified_date timestamp(6),
      content_type varchar(255) not null,
-     img_url_mobile varchar(255) not null,
-     img_url_pc varchar(255) not null,
+    mobileImageUrl varchar(255) not null,
+    pcImageUrl varchar(255) not null,
      link varchar(255),
      location varchar(255) not null,
      end_date date not null,
@@ -15,43 +15,43 @@ CREATE TABLE if not exists banners (
 
 DELETE FROM banners;
 
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2031-06-10', '2031-06-17','user');
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2031-06-01', '2031-06-30','user');
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2031-01-01', '2031-06-30','user');
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2030-01-01', '2030-12-31','user');
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2030-01-01', '2030-06-30','user');
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2030-01-01', '2030-12-31','user');
 
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2024-12-01', '2024-12-31','user');
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2024-12-01', '2025-12-01','user');
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2024-01-01', '2025-06-30','user');
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2024-06-30', '2025-06-30','user');
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2024-06-30', '2025-01-01','user');
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2024-01-01', '2025-01-01','user');
 
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2011-06-10', '2011-06-17','user');
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2011-06-01', '2011-06-30','user');
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2011-01-01', '2011-06-30','user');
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2010-01-01', '2011-12-31','user');
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2010-01-01', '2010-06-30','user');
-INSERT INTO banners (content_type, img_url_mobile, img_url_pc, location, start_date, end_date, publisher)
+INSERT INTO banners (content_type, mobileImageUrl, pcImageUrl, location, start_date, end_date, publisher)
 VALUES ('PRODUCT', '','','PLAYGROUND_COMMUNITY','2010-01-01', '2010-12-31','user');
 
 
