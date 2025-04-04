00564258dd00c8250418fb4b05b1583390f77c49
softmoca <acyc0909@gmail.com>
2025-04-05 01:45:20 +0900

    [FIX] dev 도메인 cors 추가

--- operation-api/src/main/java/org/sopt/makers/operation/common/config/WebConfig.java
+++ operation-api/src/main/java/org/sopt/makers/operation/common/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://admins.sopt.org")
+                .allowedOrigins("https://admins.sopt.org",      "https://operation.api.dev.sopt.org")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
