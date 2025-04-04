89b70d63022095cdf059de104a1b6a3492271294
softmoca <acyc0909@gmail.com>
2025-04-05 01:43:48 +0900

    [FIX] 출석시작하기 PAtCh CORS 수정

---
+++ operation-api/src/main/java/org/sopt/makers/operation/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package org.sopt.makers.operation.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://admins.sopt.org")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
