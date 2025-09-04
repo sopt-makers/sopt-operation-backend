0b8cdb3789091345b26a93f97608c9591629e7ec
softmoca <acyc0909@gmail.com>
2025-09-04 22:01:30 +0900

    fix : 직렬화 추가

---
+++ operation-common/src/main/java/org/sopt/makers/operation/config/JacksonConfig.java
@@ -0,0 +1,19 @@
+package org.sopt.makers.operation.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+}
