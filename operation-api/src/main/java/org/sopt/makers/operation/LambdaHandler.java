0fa341d53213826e5b8ad503176b442a4ec316ba
softmoca <acyc0909@gmail.com>
2026-01-14 14:45:39 +0900

    feat: LambdaHandler 클래스 생성
    - AWS Lambda 진입점 핸들러 구현
    - SpringBootLambdaContainerHandler를 통해 Spring Boot 연동
    - API Gateway 요청을 Spring Controller로 라우팅
    - SnapStart 지원을 위한 static 초기화 블록 구성

---
+++ operation-api/src/main/java/org/sopt/makers/operation/LambdaHandler.java
@@ -0,0 +1,39 @@
+package org.sopt.makers.operation;
+
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * AWS Lambda 진입점 핸들러
+ *
+ * API Gateway 요청을 Spring Boot Controller로 라우팅합니다.
+ * SnapStart를 통해 Cold Start 시간을 최소화합니다.
+ */
+public class LambdaHandler implements RequestStreamHandler {
+
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(OperationApplication.class);
+        } catch (ContainerInitializationException e) {
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        } catch (Exception e) {
+            throw new RuntimeException("Could not initialize Lambda handler", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
+            throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+    }
+}
